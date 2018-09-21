04818c9fc91fe8cbb015eb67ea4f5d2278a38acd
Mew22 <sebastien.delaherche@gmail.com>
2018-09-22 00:58:46 +0200

    Modify about page to integrate data providers

--- Chronique/Chronique/ViewModels/AboutViewModel.cs
+++ Chronique/Chronique/ViewModels/AboutViewModel.cs
@@ -16,6 +16,10 @@ namespace Chronique.ViewModels
             OpenFacebookCommand = new Command(() => Device.OpenUri(new Uri("https://www.facebook.com/lachroniquemag/")));
             OpenYoutubeCommand = new Command(() => Device.OpenUri(new Uri("https://www.youtube.com/channel/UCY12z7kBOgBScXdj9W7ZBVg")));
             OpenSpotifyCommand = new Command(() => Device.OpenUri(new Uri("http://open.spotify.com/user/lachroniquemag")));
+
+            OpenLastFmCommand = new Command(() => Device.OpenUri(new Uri("https://www.last.fm/fr/home")));
+            OpenMbCommand = new Command(() => Device.OpenUri(new Uri("https://musicbrainz.org/")));
+            OpenSongkickCommand = new Command(() => Device.OpenUri(new Uri("https://www.songkick.com/home")));
         }
 
         public ICommand OpenWebCommand { get; }
@@ -23,5 +27,9 @@ namespace Chronique.ViewModels
         public ICommand OpenFacebookCommand { get; }
         public ICommand OpenYoutubeCommand { get; }
         public ICommand OpenSpotifyCommand { get; }
+
+        public ICommand OpenLastFmCommand { get; }
+        public ICommand OpenMbCommand { get; }
+        public ICommand OpenSongkickCommand { get; }
     }
 }
