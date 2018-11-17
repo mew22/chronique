74987fcc8fd9f328f96d523a568bb3e626d2c1f4
mew22 <sebastien.delaherche@gmail.com>
2018-11-18 00:53:13 +0100

    Fix messenging with platform specific code

---
+++ Chronique/Chronique.Android/MessageAndroid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Chronique.Droid;
+using Chronique.Layout;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MessageAndroid))]
+namespace Chronique.Droid
+{
+
+
+        public class MessageAndroid : IMessageToast
+        {
+            public void LongAlert(string message)
+            {
+                Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+            }
+
+            public void ShortAlert(string message)
+            {
+                Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+            }
+        }
+
+}
